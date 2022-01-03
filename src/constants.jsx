829996d4078f47e8029b7cb0c3f310ad7c61cbcb
Arpit Ingle <arpitingle1@gmail.com>
2022-01-03 22:39:30 +0530

    Add files via upload

---
+++ src/constants.jsx
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = '0xaE2181bA9982C2a221e94cC50eA71359944b1aA9';
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
